ed628881835b3615e2f25bbd38a4f93a6fd9a7b4
Mészáros Zsolt <h879678@stud.u-szeged.hu>
2020-11-02 10:06:02 +0100

    Building add creates

---
+++ ingatlanMaven/src/main/java/hu/zsolt/ingatlan/IngatlanApplication.java
@@ -0,0 +1,16 @@
+package hu.zsolt.ingatlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngatlanApplication {
+
+	public static void main(String[] args) { SpringApplication.run(IngatlanApplication.class, args);
+
+	System.out.println("hello word");
+
+		}
+	}
+
+
---
+++ ingatlanMaven/src/test/java/hu/zsolt/ingatlan/IngatlanApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.zsolt.ingatlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngatlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
