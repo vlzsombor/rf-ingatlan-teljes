55b39e8b793e51787184f0fba1b0cca5cd92e11d
Böröc Tamás <h850793@stud.u-szeged.hu>
2020-11-04 18:07:07 +0100

    #45 AdminController.java
    Little changes in code.

--- src/main/java/hu/IB153I16/ingatlan/controller/AdminController.java
+++ src/main/java/hu/IB153I16/ingatlan/controller/AdminController.java
@@ -1,20 +1,16 @@
 package hu.IB153I16.ingatlan.controller;
 
-import hu.IB153I16.ingatlan.repository.RealEstateRepository;
 import hu.IB153I16.ingatlan.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.stream.Collectors;
-
 @Controller
 @RequestMapping("admin")
 public class AdminController {
+
     @Autowired
     private UserRepository userRepository;
 
@@ -23,12 +19,10 @@ public class AdminController {
         return "admin/index";
     }
 
-    @GetMapping("/getusers")
+    @GetMapping("/getUsers")
     public String getUsers(Model model) {
-
         var users = userRepository.findAll();
         model.addAttribute("users",users);
-
         return "profile/getUsers";
     }
 }
